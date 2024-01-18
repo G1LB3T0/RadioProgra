3bf672abf6f2c165fa28e008d881f9f9aa6134d9
G1LB3T0 <127223507+G1LB3T0@users.noreply.github.com>
2024-01-18 12:31:25 -0600

    Add files via upload

---
+++ Radio1/src/Controlador.java
@@ -0,0 +1,50 @@
+public class Controlador implements IRadio{
+    public void On() {
+
+    }
+
+    @Override
+    public boolean getState() {
+        return false;
+    }
+
+    @Override
+    public void tooglePowerOffOn() {
+
+    }
+
+    @Override
+    public void toogleAMFM() {
+
+    }
+
+    @Override
+    public boolean getStateAMFM() {
+        return false;
+    }
+
+    @Override
+    public void nextFrequency() {
+
+    }
+
+    @Override
+    public void previousFrequency() {
+
+    }
+
+    @Override
+    public float getCurrentFrequency() {
+        return 0;
+    }
+
+    @Override
+    public void setFavFrequency(int button) {
+
+    }
+
+    @Override
+    public float getFavFrequency(int button) {
+        return 0;
+    }
+}
