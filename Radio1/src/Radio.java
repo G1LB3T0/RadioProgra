01e2bc2362851a013de750b9fdd990aa20a63b4c
Luis Gonzalez 23353 <luis.yo09@hotmail.com>
2024-01-18 18:29:37 -0600

    creacion de clase radio

---
+++ Radio1/src/Radio.java
@@ -0,0 +1,29 @@
+public class Radio {
+    boolean power;
+    double emisora;
+    String frecuencia;
+
+    public boolean isPower() {
+        return power;
+    }
+
+    public void setPower(boolean power) {
+        this.power = power;
+    }
+
+    public double getEmisora() {
+        return emisora;
+    }
+
+    public void setEmisora(double emisora) {
+        this.emisora = emisora;
+    }
+
+    public String getFrecuencia() {
+        return frecuencia;
+    }
+
+    public void setFrecuencia(String frecuencia) {
+        this.frecuencia = frecuencia;
+    }
+}
